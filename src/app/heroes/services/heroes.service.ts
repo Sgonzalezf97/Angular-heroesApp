da377366b9b67a1c29910ad3333cf5c606289470
Sebastian Gonzalez <jusegofo@gmail.com>
2024-02-09 17:02:02 -0500

    Add heroes service

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface' ;
+import { Observable, catchError, of } from 'rxjs';
+import { environments } from '../../../environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+  constructor(private http: HttpClient) { }
+
+  private baseUrl: string= environments.baseUrl;
+
+  getHeros():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById(id: string):Observable<Hero | undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(error => of(undefined)) //se pone el of para volver el undefined un observable
+    )
+  }
+}
