f1f9d2b2187bee264dcd6a7693d4c7ceca6f0d99
Sebastian Gonzalez <jusegofo@gmail.com>
2024-02-11 22:49:21 -0500

    Update and delete service

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hero } from '../interfaces/hero.interface' ;
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -23,5 +23,23 @@ export class HeroesService {
 
   getSuggestions(query:string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`,hero)
+  }
+
+  updateHero(hero: Hero): Observable<Hero>{
+    if (!hero.id) {
+      throw Error('Hero id is requiered')
+    }
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`,hero)
+  }
+  deleteHero(id: string): Observable<boolean>{
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(err => of(false)),
+      map(resp => true)
+    )
   }
 }
